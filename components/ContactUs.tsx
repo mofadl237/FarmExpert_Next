8bd7e78022a699f88cf1d69218e73ecb10d20c4d
mofadl237 <mofadl237@gmail.com>
2025-07-05 03:03:05 +0300

    Enhanced UI

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -11,9 +11,9 @@ const ContactUs = () => {
       const isArabic = pathname.startsWith('/ar');
   return (
     <div className="bg-gradient-to-tr from-[#68CD75] to-[#2DB683] w-full h-screen flex justify-center items-center">
-          <div className={`relative  w-[80%]    md:h-[75%] bg-[url('/main-2.jpg')]  rounded-[4px] shadow-md bg-top`}>
+          <div className={`relative  w-[80%]  h-[70%]   md:h-[75%] bg-[url('/main-2.jpg')]  rounded-[4px] shadow-md bg-top`}>
             <div
-              className={`absolute    h-full w-full md:w-[60%] bg-[rgba(255,255,255,0.8)]  rounded-[4px] z-10 ${isArabic && 'end-0'} `}
+              className={`absolute    h-[100%] w-full md:w-[60%] bg-[rgba(255,255,255,0.6)]  rounded-[4px] z-10 ${isArabic && 'end-0'} `}
               style={{ clipPath: "polygon(0 0, 100% 0, 80% 100%, 0 100%)" }}
             >
               <AnimatedHeader title={t("contact.title")} center />
