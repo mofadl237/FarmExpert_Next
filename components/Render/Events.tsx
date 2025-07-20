fc461359ab046bb336b367ae612f4e95638b759b
mofadl237 <mofadl237@gmail.com>
2025-07-20 22:18:40 +0300

    Style Event Cattle

--- components/Render/Events.tsx
+++ components/Render/Events.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useGetEventsQuery } from '@/store/services/ManagerFarm'
 import EventAction from '../Action/EventAction';
-
+import './Event.scss'
 
 function Events() {
     const {data:Events,isLoading} = useGetEventsQuery();
@@ -13,11 +13,35 @@ function Events() {
   <div className='grid gap-3  grid-cols-[repeat(auto-fit,minmax(250px,1fr))]'>
     {Events && Events.map((event)=>(
         // console.log("Event ====> ",event)
-        // Add Class Color For Every Event
-        <div key={event.id} className={`border-2 rounded-md p-4 space-y-3`}>
-            <h1>Event Type : {event.eventType}</h1>
-            <h1>Tag Number : {event.tagNumber}</h1>
+        // Add Class Color For Every Event 
+        <div key={event.id} className={`border-2 rounded-md p-4 space-y-3 ${event.eventType.split(" ")[0]}`}>
+            <h2>Event Type : {event.eventType}</h2>
+            <h3>Tag Number : {event.tagNumber}</h3>
             {/* Valid For Every Event */}
+            {(event.eventType === 'Treated') && (
+              <div>
+                <p>Medicine : {event.medicine} </p>
+                <p>Dosage : {event.dosage}</p>
+                <p>WithdrawalTime : {event.withdrawalTime}</p>
+              </div>
+              
+            )}
+            {(event.eventType === 'Weighted') && (
+              <div>
+                <p>Weighted : {event.weight} </p>
+              </div>
+            )}
+            {(event.eventType === 'Gives Birth') && (
+              <div>
+                <p>CalfGender : {event.calfGender}</p>
+              </div>
+            )}
+            {(event.eventType === 'Vaccinated') && (
+              <div>
+                <p>Vaccine Type : {event.vaccineType}</p>
+              </div>
+            )}
+            <p>Notes : {event.notes}</p>
             <EventAction Event={event} />
            
         </div>
